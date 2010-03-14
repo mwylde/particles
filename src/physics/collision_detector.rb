8abd436d2cf46c453c523591747700ffdaff26c7
Eli <eli@elilies.com>
2010-03-13 21:23:17 -0500

    Start work on JP's method of collision detection

--- src/physics/collision_detector.rb
+++ src/physics/collision_detector.rb
@@ -1,4 +1,63 @@
 module CollisionDetector
+  module JP
+    def detect_and_correct_collisions(objs)
+      collision_happened = false
+      for a in objs
+        for b in objs
+          if a != b && collided?(a, b)
+            collision_happened = true
+            # now fix it
+          end
+        end
+      end while collision_happened
+    end
+    
+    def collided?(a, b):
+      r_sum = a.radius + b.radius
+      
+      a_new = [a.x, a.y]
+      a_old = [a.x - a.xv, a.y - a.yv]
+      
+      b_new = [b.x, b.y]
+      b_old = [b.x - b.xv, b.y - b.yv]
+    	
+    	return intersection?(a_old, a_new, b_old, b_new) 
+    	    || dist(a_new, b_new, b_old) < r_sum
+    	    || dist(a_old, b_new, b_old) < r_sum
+    	    || dist(b_new, a_new, a_old) < r_sum
+    	    || dist(b_old, a_new, a_old) < r_sum
+    end
+
+    def intersection?(ao, an, bo, bn):
+    	denominator = (bn[1]-bo[1])*(an[0]-ao[0]) - (bn[0]-bo[0])*(an[1]-ao[1])
+    	
+    	u1=((bn[0]-bo[0])*(ao[1]-bo[1]) - (bn[1]-bo[1])*(ao[0]-bo[1]))/denominator
+    	return false if u1<0 || u1>1
+
+    	u2=((an[0]-ao[0])*(ao[1]-bo[1]) - (an[1]-ao[1])*(ao[0]-bo[1]))/denominator
+    	return u2 >= 0 && u2 <= 1
+  	end
+  	
+  	def dist(pt, start, finish)  	  
+      r_numerator = (pt[0]-start[0])*(finish[0]-start[0]) +
+                    (pt[1]-start[1])*(finish[1]-start[1])
+      
+      r_denomenator = (finish[0]-start[0])*(finish[0]-start[0]) +
+                      (finish[1]-start[1])*(finish[1]-start[1])
+      
+      r = r_numerator / r_denomenator
+      
+      s = ((start[1]-pt[1])*(finish[0]-start[0])-(start[0]-pt[0])*(finish[1]-start[1]) ) / r_denomenator
+
+      if (r >= 0) && (r <= 1)
+          return Math.abs(s)*Math.sqrt(r_denomenator)
+      else
+          return [(pt[0]-start[0] )**2 + (pt[1]-start[1] )**2,
+                  (pt[0]-finish[0])**2 + (pt[1]-finish[1])**2].min
+      end
+	  end
+  	
+  end
   module Posteriori    
     def detect_and_correct_collisions(objs)
       collision_happened = false
